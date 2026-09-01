af4238c34376021ec2b8ee7af0d6d19611111489
Mohamed Eslam ShamsElden <medoeslam522@gmail.com>
2026-09-01 13:24:52 +0300

    Fix folder structure and update navigation links

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+}
+document.addEventListener("DOMContentLoaded", init);
